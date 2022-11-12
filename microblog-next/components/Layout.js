f25aa96e316e817ebc0f4fdcc6d96ad8650af681
sc30gsw <haidoushi875@gmail.com>
2022-11-13 06:59:44 +0900

    CSSの名称変更

--- microblog-next/components/Layout.js
+++ microblog-next/components/Layout.js
@@ -1,19 +1,19 @@
 import Head from "next/head";
-import styles from "./layout.module.css";
-import utilStyles from "../styles/utils.module.css";
+import style from "./layout.module.css";
+import utilStyle from "../styles/utils.module.css";
 
 const name = "Shin code";
 export const siteTitle = "Next.js blog";
 
 function Layout({ children }) {
 	return (
-		<div className={styles.container}>
+		<div className={style.container}>
 			<Head>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<header className={styles.header}>
-				<img src="/images/profile.png" className={utilStyles.borderCircle} />
-				<h1 className={utilStyles.heading2Xl}>{name}</h1>
+			<header className={style.header}>
+				<img src="/images/profile.png" className={utilStyle.borderCircle} />
+				<h1 className={utilStyle.heading2Xl}>{name}</h1>
 			</header>
 			{/* propsを設定することで、import先でmainの中身を各自で設定できる */}
 			<main>{children}</main>
