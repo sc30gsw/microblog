4a51985a98c4de425b1b46119d762cfae22272be
sc30gsw <haidoushi875@gmail.com>
2022-11-14 16:22:10 +0900

    動的ルーティングに紐づくデータをSSGとして取得し、表示する処理の実装

--- microblog-next/pages/posts/[id].js
+++ microblog-next/pages/posts/[id].js
@@ -1,5 +1,37 @@
 import Layout from "../../components/Layout";
+import { getAllPostIds, getPostData } from "../../lib/post";
 
-export default function Post() {
-	return <Layout>動的ルーティング設定</Layout>;
+export async function getStaticPaths() {
+	const paths = getAllPostIds();
+
+	return {
+		paths,
+		fallback: false,
+	};
+}
+
+export async function getStaticProps({ params }) {
+	// paramsに動的URL（ここではid）に紐づくデータが格納されているため、
+	// getPostData()の引数に「params.id」を渡すことでidに紐づく、
+	// HTML解析がされたデータを呼び出し、取得することができる
+	// 呼び出している関数が非同期（async）のためawaitとし、非同期で呼び出している
+	const postData = await getPostData(params.id);
+
+	return {
+		props: {
+			postData,
+		},
+	};
+}
+
+export default function Post({ postData }) {
+	return (
+		<Layout>
+			{postData.title}
+			<br />
+			{postData.date}
+			<br />
+			{postData.blogContentHTML}
+		</Layout>
+	);
 }
